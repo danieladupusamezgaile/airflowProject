6b127d27f947b6fbe77b96e6cd5e2ec41563c636
Daniela Dupuša-Mežgaile <daniela.dupusamezgaile@gmail.com>
2025-10-19 13:41:09 +0300

    xcom pus n pull

--- dags/extractor.py
+++ dags/extractor.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 DATASET_COCKTAIL = Dataset("/tmp/random_cocktail.json")
 
-def _get_cocktail():
+def _get_cocktail(**context): # ti - task instance
+    ti = context["ti"]
     import requests
 
     try:
@@ -13,11 +14,17 @@ def _get_cocktail():
         r.raise_for_status()
         with open(DATASET_COCKTAIL.uri, 'wb') as f:
             f.write(r.content)
+        ti.xcom_push(key="request_size", value=len(r.content))
     except Exception as e:
         print(f"API currently not available. Error: {e}")
 
+def _check_size(**context):
+    ti = context["ti"]
+    request_size = ti.xcom_pull(task_ids='get_cocktail', key='request_size')
+    print(f"Size of the request: {request_size}")
+
 @dag(
-    start_date=datetime(2025, 10, 16),
+    start_date=datetime(2025, 10, 10),
     schedule="@daily",
     catchup=False
 )
@@ -27,4 +34,12 @@ def extractor():
         python_callable=_get_cocktail,
         outlets=[DATASET_COCKTAIL]
     )
+    
+    check_size = PythonOperator(
+        task_id='check_size',
+        python_callable=_check_size
+    )
+
+    get_cocktail >> check_size
+    
 extractor()
